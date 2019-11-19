253d82032766a9bd6ea1cbfb81602db9acea85c2
Bertram <bw-sp@live.dk>
2019-11-19 10:17:59 +0100

    Added NuGet packages

--- Microwave.Test.Integration/IT1.cs
+++ Microwave.Test.Integration/IT1.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+
 
 namespace Microwave.Test.Integration
 {
+    [TestFixture]
     public class IT1
     {
+
     }
 }
