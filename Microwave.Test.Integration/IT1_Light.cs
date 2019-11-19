92e220ad5f9559e543a01274424885c7e44b2ca4
Bertram <bw-sp@live.dk>
2019-11-19 10:48:16 +0100

    Pushing NuGet packages

--- Microwave.Test.Integration/IT1_Light.cs
+++ Microwave.Test.Integration/IT1_Light.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
 using NSubstitute;
 using NUnit.Framework;
 
@@ -12,6 +14,20 @@ namespace Microwave.Test.Integration
     [TestFixture]
     public class IT1_Light
     {
+        private Light _uut;
+        private IOutput _outputSub;
 
+        [SetUp]
+        public void SetUp()
+        {
+            _outputSub = Substitute.For<IOutput>();
+            _uut = new Light(_outputSub);
+        }
+
+        [Test]
+        public void testopgave()
+        {
+
+        }
     }
 }
